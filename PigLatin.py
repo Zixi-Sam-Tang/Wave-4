945656c86be5a138b508347a5dcbd625eb28ce5e
zixi-sam0tang <zixi.sam.tang@gmail.com>
2020-05-31 17:23:37 -0400

    Wave 4

--- PigLatin.py
+++ PigLatin.py
@@ -3,7 +3,7 @@ def piglatin(word):
         temp = ""
         vowels = ["a", "e", "i", "o", "u", "y"]
         if j[0] not in vowels:
-            for i in range(1, len(j)):
+            for i in range(0, len(j)):
                 if j[i] in vowels:
                     temp = j[i:len(j)] + j[0:i] + "ay"
                     break
